26330ffe6fc6e2efacb95259db77b6804b1543a0
Yinuo-M <mengyn1999@gmail.com>
2021-07-25 13:39:21 +0100

    Complete static components

--- src/lap-table/LapTable.js
+++ src/lap-table/LapTable.js
@@ -1,3 +1,24 @@
+import TableBody from "./TableBody";
+
 export default function LapTable() {
-  return <div>LabTable</div>;
+  return (
+    <div className="lap">
+      <table className="lap__table">
+        <thead className="lap__thead">
+          <tr className="lap__tr-head">
+            <th className="lap__th" scope="col">
+              Lap
+            </th>
+            <th className="lap__th" scope="col">
+              Lap time
+            </th>
+            <th className="lap__th" scope="col">
+              Total time
+            </th>
+          </tr>
+        </thead>
+        <TableBody />
+      </table>
+    </div>
+  );
 }
