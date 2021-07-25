7a2940359f932631e97e02a611e119ca20210224
Yinuo-M <mengyn1999@gmail.com>
2021-07-25 18:08:03 +0100

    Improve responsiveness

--- src/common/Button.js
+++ src/common/Button.js
@@ -2,14 +2,12 @@ import "./Button.scss";
 
 export default function Button(props) {
   return (
-    <div className={`button ${props.className}`}>
-      <button
-        type="button"
-        className="button__elem"
-        onClick={props.handleClick}
-      >
-        {props.text}
-      </button>
-    </div>
+    <button
+      type="button"
+      className={`button__elem ${props.className}`}
+      onClick={props.handleClick}
+    >
+      {props.text}
+    </button>
   );
 }
